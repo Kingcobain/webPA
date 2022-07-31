c0a9b9c2e9d19ebace0b52d7be2df27938d0f5e1
Kingcobain <rafiuletta@gmail.com>
2022-08-01 03:41:41 +0700

    up

--- resources/views/dashboard/categories/edit.blade.php
+++ resources/views/dashboard/categories/edit.blade.php
@@ -23,43 +23,6 @@
                 <label for="slug" class="form-label">Slug</label>
                 <input type="text" class="form-control" id="slug" name="slug" disable readonly value="{{ old('slug', $category->slug) }}">
             </div>
-            {{-- <div class="mb-3">
-                <label for="category" class="form-label">Category</label>
-                <select class="form-select" name="category_id">
-                    @foreach ($categories as $category)
-                        @if(old('category_id, $post->category_id') == $category->id)
-                            <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
-                        @else
-                            <option value="{{ $category->id }}">{{ $category->name }}</option>
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="mb-3">
-                <label for="image" class="form-label">Post Image</label>
-                <input type="hidden" name="oldImage" value="{{ $post->image }}">
-                @if($post->image)
-                <img src="{{ asset('storage/' . $post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
-                @else
-                <img class="img-preview img-fluid mb-3 col-sm-5">
-                @endif                
-                <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
-                @error('image')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div class="mb-3">
-                <label for="body" class="form-label">body</label>
-                @error('body')
-                    <p class="text-danger">{{ $message }}</p>
-                @enderror
-                <input id="body" type="hidden" name="body" value="{{ old('body', $post->body) }}">
-                <trix-editor input="body"></trix-editor>
-            </div> --}}
 
             <button type="submit" class="btn btn-primary">Update Category</button>
         </form>
@@ -70,29 +33,11 @@
         const slug = document.querySelector('#slug');
 
         name.addEventListener('change', function() {
-            fetch('/dashboard/posts/checkSlug?name=' + name.value)
+            fetch('/dashboard/categories/checkSlug?name=' + name.value)
             .then(response => response.json())
             .then(data => slug.value = data.slug)
         });
 
-        // document.addEventListener('trix-file-accept', function(e) {
-        //     e.preventDefault();
-        // })
-
-        // function previewImage() {
-
-        // const image = document.querySelector('#image');
-        // const imgPreview = document.querySelector('.img-preview');
-
-        // imgPreview.style.display = 'block';
-
-        // const oFReader = new FileReader();
-        // oFReader.readAsDataURL(image.files[0]);
-
-        // oFReader.onload = function(oFREvent) {
-        //     imgPreview.src = oFREvent.target.result;
-        //     }
-        // }
     </script>
 
 @endsection
